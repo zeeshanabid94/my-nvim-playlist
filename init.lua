66669984db5c67a420a71bad9e5b1b32adaf268f
Zeeshan Abid <zeeshanabid94@gmail.com>
2024-06-03 07:05:21 +0000

    My personal nvim configuration that I can take anywhere.

---
+++ init.lua
@@ -0,0 +1,21 @@
+require("plugins.init")
+require("remap")
+print("Welcome to the masterclass")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup(plugins, opts)
+
+-- Setup color scheme
+vim.cmd.colorscheme("tokyonight")
+
---
+++ lua/plugins/init.lua
@@ -0,0 +1,23 @@
+plugins = {
+	{
+    		'nvim-telescope/telescope.nvim', tag = '0.1.6',
+		-- or                              , branch = '0.1.x',
+      		dependencies = { 'nvim-lua/plenary.nvim' }
+    	},
+	{
+  		"folke/tokyonight.nvim",
+  		lazy = false,
+  		priority = 1000,
+  		opts = {},
+	},
+	{
+		'nvim-treesitter/nvim-treesitter',
+		cmd = "TSUpdate"
+	},
+	{
+    		"ThePrimeagen/harpoon",
+    		branch = "harpoon2",
+    		dependencies = { "nvim-lua/plenary.nvim" }
+	}
+}
+
