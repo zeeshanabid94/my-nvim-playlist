97f2ea4646de965a37efc68eb0e9bd0c2776e78f
Zeeshan Abid <zeeshanabid94@gmail.com>
2024-06-04 01:27:32 +0000

    Install CoC plugin with build command
    Install CoC plugin and has build command to install
    CoC Language Servers for rust, typescript and json.

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,8 +1,8 @@
 plugins = {
 	{
-    		'nvim-telescope/telescope.nvim', tag = '0.1.6',
-		-- or                              , branch = '0.1.x',
-      		dependencies = { 'nvim-lua/plenary.nvim' }
+    		"nvim-telescope/telescope.nvim", tag = "0.1.6",
+		-- or                              , branch = "0.1.x",
+      		dependencies = { "nvim-lua/plenary.nvim" }
     	},
 	{
   		"folke/tokyonight.nvim",
@@ -11,13 +11,18 @@ plugins = {
   		opts = {},
 	},
 	{
-		'nvim-treesitter/nvim-treesitter',
+		"nvim-treesitter/nvim-treesitter",
 		cmd = "TSUpdate"
 	},
 	{
     		"ThePrimeagen/harpoon",
     		branch = "harpoon2",
     		dependencies = { "nvim-lua/plenary.nvim" }
+	},
+	{
+		"neoclide/coc.nvim",
+		branch = "release",
+		build = ":CocInstall coc-json coc-tsserver coc-rust-analyzer"
 	}
 }
 
